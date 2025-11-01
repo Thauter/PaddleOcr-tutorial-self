b02d736334aa7c87dda1e13ab1c1e55af18cacfe
thauter <git@thauter.com>
2025-11-01 17:30:00 +0800

    feat: Add baseOcr_output.en.py

---
+++ baseOcr_output.en.py
@@ -0,0 +1,12 @@
+from paddleocr import PaddleOCRVL
+
+# Configure to use the vLLM server
+pipeline = PaddleOCRVL(
+    vl_rec_backend="vllm-server",
+    vl_rec_server_url="http://localhost:8111/v1"
+)
+
+result = pipeline.predict("images/2501.10973.png")
+for res in result:
+    res.print()
+
